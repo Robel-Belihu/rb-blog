43e56ee373e80feebf2d3cd39631d6b73502fb85
Robel-Belihu <robelmelalku334@gmail.com>
2023-05-03 16:38:30 +0300

    some more navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
 import HomePage from "./pages/HomePage";
 import CreatePostPage from "./pages/CreatePostPage";
 import Login from "./pages/Login";
+import { auth } from "./firebase";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const [isLogged, setIsLogged] = useState(false);
+  const navigate = useNavigate();
+
+  const logout = () => {
+    signOut(auth);
+    localStorage.clear();
+    isLogged(false);
+    navigate("/login");
+  };
   return (
     <Router>
       <nav class="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full">
@@ -20,18 +33,27 @@ function App() {
           >
             Create a Post
           </Link>
-          <Link
-            className="text-white text-lg bg-green-500 rounded-md py-1 px-2 text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2"
-            to="/login"
-          >
-            Login
-          </Link>
+          {!isLogged ? (
+            <Link
+              className="text-white text-lg bg-green-500 rounded-md py-1 px-2 text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2"
+              to="/login"
+            >
+              Login
+            </Link>
+          ) : (
+            <button
+              onClick={logout}
+              className="text-white text-lg bg-red-500 rounded-md px-2 text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2"
+            >
+              Logout
+            </button>
+          )}
         </div>
       </nav>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/createPost" element={<CreatePostPage />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login setIsLogged={setIsLogged} />} />
       </Routes>
     </Router>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { auth, provider } from "../firebase";
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
-function Login() {
-  return <div>Login</div>;
+function Login({ setIsLogged }) {
+  const navigate = useNavigate();
+
+  const signIn = () => {
+    signInWithPopup(auth, provider).then((res) => {
+      localStorage.setItem("isLogged", true);
+      setIsLogged(true);
+      navigate("/");
+    });
+  };
+
+  return (
+    <div className="container flex flex-col items-center justify-center gap-3">
+      <p>Sign in with Google</p>
+      <button
+        onClick={signIn}
+        className="btn text-white bg-blue-500 py-1 px-3 text-xl cursor-pointer"
+      >
+        Sign in with Google
+      </button>
+    </div>
+  );
 }
 
 export default Login;
